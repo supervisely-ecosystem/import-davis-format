5e731f47d453e3e2aa5feff19459c4a97cc8db4e
Alex Emelyanov <alex84_84_84@mail.ru>
2021-06-16 17:28:35 +0300

    start project

--- README.md
+++ README.md
@@ -1,2 +1,78 @@
-# import-davis-format
-import-davis-format
+<div align="center" markdown>
+<img src="https://i.imgur.com/48JFbtt.png" width="1900px"/>
+
+
+# Import OVIS
+
+<p align="center">
+  <a href="#Overview">Overview</a> •
+  <a href="#Preparation">Preparation</a> •
+  <a href="#How-To-Run">How To Run</a> •
+  <a href="#How-To-Use">How To Use</a>
+</p>
+
+
+[![](https://img.shields.io/badge/slack-chat-green.svg?logo=slack)](https://supervise.ly/slack)
+![GitHub release (latest SemVer)](https://img.shields.io/github/v/release/supervisely-ecosystem/import-ovis-format)
+[![views](https://app.supervise.ly/public/api/v3/ecosystem.counters?repo=supervisely-ecosystem/import-ovis-format&counter=views&label=views)](https://supervise.ly)
+[![used by teams](https://app.supervise.ly/public/api/v3/ecosystem.counters?repo=supervisely-ecosystem/import-ovis-format&counter=downloads&label=used%20by%20teams)](https://supervise.ly)
+[![runs](https://app.supervise.ly/public/api/v3/ecosystem.counters?repo=supervisely-ecosystem/import-ovis-format&counter=runs&label=runs&123)](https://supervise.ly)
+
+</div>
+
+## Overview
+
+Import data in [OVIS](http://songbai.site/ovis/) format to [Supervisely](https://supervise.ly/) from folder or `tar` archive.
+
+## Preparation
+
+Upload your data in `OVIS` format to `Team Files` (for example you can create `import_ovis` folder). You can also upload data from `.tar` archive.
+
+<img src="https://i.imgur.com/45uOaK0.png"/>
+
+#### Structure of directory or archive have to be the following:   
+```
+.
+├── train.zip
+│   ├── 0b4b662c
+|   |	├── img_0000001.jpg
+|   |	├── img_0000002.jpg
+|   |	├── ...
+│   ├── 2f477d05
+|   |	├── img_0000001.jpg
+|   |	├── img_0000002.jpg
+|   |	├── ...
+│   └── ...
+│    
+└── annotations_train.json
+```
+
+#### Note:
+
+If you will drag and drop archive with parent directory instead of its content, import will crash.
+
+Annotations file must contain `annotations` in its name. The images archive must have a name that matches the line in the annotation file name after the `_` and before the extension. If you do not comply with these conditions import will crash.
+
+## How To Run 
+
+**Step 1**: Add app to your team from [Ecosystem](https://ecosystem.supervise.ly/apps/import-cityscapes) if it is not there.
+
+**Step 2**: Go to `Current Team`->`Files` page, right-click on your `.tar` archive or `folder`, containing `ovis` data and choose `Run App`->`Import Ovis`. You will be redirected to `Workspace`->`Tasks` page. 
+
+<img src="https://i.imgur.com/dJr5sLz.png"/>
+
+**Step 3**: Select slider value to split images in `train` folders with `train` and `val` tags.
+
+<img src="https://i.imgur.com/vXWYwhR.png" width="600px"/>
+
+
+
+## How to use
+
+Resulting project will be placed to your current `Workspace` with the same name as the `ovis` archive. Videos in datasets will have tags (`train`, `val`) corresponding to the ratio, exposed in the slider.
+
+<img src="https://i.imgur.com/UC0ygAH.png"/>
+
+You can also access your project by clicking on it's name from `Tasks` page.
+
+<img src="https://i.imgur.com/h54uGur.png">
---
+++ src/main.py
@@ -0,0 +1,205 @@
+import os, random
+import zipfile, json, tarfile
+import supervisely_lib as sly
+import glob
+from supervisely_lib.io.fs import get_file_name, get_file_name_with_ext
+from pathlib import Path
+import shutil
+import cv2
+from collections import defaultdict
+from supervisely_lib.video_annotation.video_tag import VideoTag
+from supervisely_lib.video_annotation.video_tag_collection import VideoTagCollection
+from PIL import Image
+import numpy as np
+
+
+my_app = sly.AppService()
+TEAM_ID = int(os.environ['context.teamId'])
+WORKSPACE_ID = int(os.environ['context.workspaceId'])
+INPUT_DIR = os.environ.get("modal.state.slyFolder")
+INPUT_FILE = os.environ.get("modal.state.slyFile")
+PROJECT_NAME = 'DAVIS'
+logger = sly.logger
+#archive_ext = '.zip'
+frame_rate = 5
+video_ext = '.mp4'
+train_tag = 'train'
+val_tag = 'val'
+
+# test = Image.open('/home/andrew/alex_work/app_data/data/davis_data/00000.png')
+# colors = test.getcolors()
+# palette = test.palette
+# a = np.asarray(test)
+# rgb = test.convert('RGB').getcolors()
+# a=0
+
+
+@my_app.callback("import_davis")
+@sly.timeit
+def import_davis(api: sly.Api, task_id, context, state, app_logger):
+
+    storage_dir = my_app.data_dir
+
+    if INPUT_DIR:
+        cur_files_path = INPUT_DIR
+        extract_dir = os.path.join(storage_dir, str(Path(cur_files_path).parent).lstrip("/"))
+        input_dir = os.path.join(extract_dir, Path(cur_files_path).name)
+        archive_path = os.path.join(storage_dir, cur_files_path.split("/")[-2] + ".tar")
+        project_name = Path(cur_files_path).name
+    else:
+        cur_files_path = INPUT_FILE
+        extract_dir = os.path.join(storage_dir, get_file_name(cur_files_path))
+        archive_path = os.path.join(storage_dir, get_file_name_with_ext(cur_files_path))
+        project_name = get_file_name(INPUT_FILE)
+        input_dir = extract_dir
+
+    # api.file.download(TEAM_ID, cur_files_path, archive_path)
+    #
+    # if tarfile.is_tarfile(archive_path):
+    #     with tarfile.open(archive_path) as archive:
+    #         archive.extractall(extract_dir)
+    # else:
+    #     raise Exception("No such file".format(INPUT_FILE))
+
+
+    for curr_arch in os.listdir(input_dir):
+        pass
+
+    a=0
+
+    search_anns = os.path.join(input_dir, "annotations_*.json")
+    anns_fine_paths = glob.glob(search_anns)
+    if len(anns_fine_paths) == 0:
+        logger.warn('There is no annotations in input data. Check your input format')
+    anns_fine_paths.sort()
+
+    ovis_classes = {}
+    id_to_obj_classes = {}
+
+    new_project = api.project.create(WORKSPACE_ID, project_name, type=sly.ProjectType.VIDEOS,
+                                     change_name_if_conflict=True)
+
+    tag_meta_train = sly.TagMeta(train_tag, sly.TagValueType.NONE)
+    tag_meta_val = sly.TagMeta(val_tag, sly.TagValueType.NONE)
+    tag_collection = sly.TagMetaCollection([tag_meta_train, tag_meta_val])
+    meta = sly.ProjectMeta(tag_metas=tag_collection)
+    api.project.update_meta(new_project.id, meta.to_json())
+
+    for ann_path in anns_fine_paths:
+        ann_name = str(Path(ann_path).name)
+        arch_name = sly.fs.get_file_name(ann_name).split('_')[1] + archive_ext
+        arch_path = os.path.join(input_dir, arch_name)
+        if not sly.fs.file_exists(arch_path):
+            logger.warn('There is no archive {} in the input data, but it must be'.format(arch_name))
+            continue
+
+        if zipfile.is_zipfile(arch_path):
+            with zipfile.ZipFile(arch_path, 'r') as archive:
+                archive.extractall(input_dir)
+        else:
+            raise Exception("No such file".format(archive_path))
+
+        imgs_dir_path = os.path.join(input_dir, sly.fs.get_file_name(arch_name))
+        ann_json = sly.json.load_json_file(ann_path)
+
+        videos = ann_json['videos']
+        ovis_anns = ann_json['annotations']
+        if not ovis_anns:
+            logger.warn('There is no annotations data in {}'.format(ann_name))
+            continue
+
+        for category in ann_json['categories']:
+            if category['id'] not in ovis_classes.keys():
+                ovis_classes[category['id']] = category['name']
+                id_to_obj_classes[category['id']] = sly.ObjClass(category['name'], sly.Bitmap)
+            else:
+                if ovis_classes[category['id']] != category['name']:
+                    logger.warn(
+                        'Category with id {} corresponds to the value {}, not {}. Check your input annotations'.format(
+                            category['id'], ovis_classes[category['id']], category['name']))
+
+        new_dataset = api.dataset.create(new_project.id, sly.fs.get_file_name(ann_name), change_name_if_conflict=True)
+        new_meta = sly.ProjectMeta(sly.ObjClassCollection(list(id_to_obj_classes.values())))
+        meta = meta.merge(new_meta)
+        api.project.update_meta(new_project.id, meta.to_json())
+
+        anns = defaultdict(list)
+        for ovis_ann in ovis_anns:
+            anns[ovis_ann['video_id']].append([ovis_ann['category_id'], ovis_ann['id'], ovis_ann['segmentations']])
+
+        for video_data in videos:
+            no_image = False
+            curr_anns = anns[video_data['id']]
+            video_objects = {}
+            for curr_ann in curr_anns:
+                video_objects[curr_ann[1]] = sly.VideoObject(id_to_obj_classes[curr_ann[0]])
+
+            #=============================create video===============================================================
+            img_size = (video_data['width'], video_data['height'])
+            video_folder = video_data['file_names'][0].split('/')[0]
+            video_name = video_folder + video_ext
+            images_path = os.path.join(imgs_dir_path, video_folder)
+            if not sly.fs.dir_exists(images_path):
+                logger.warn('There is no folder {} in the input data, but it is in annotation'.format(images_path))
+                continue
+            images = os.listdir(images_path)
+            progress = sly.Progress('Create video', len(videos), app_logger)
+            video_path = os.path.join(extract_dir, video_name)
+            video = cv2.VideoWriter(video_path, cv2.VideoWriter_fourcc(*'MP4V'), frame_rate, img_size)
+            for curr_ovis_im_path in video_data['file_names']:
+                curr_im_path = os.path.join(imgs_dir_path, curr_ovis_im_path)
+                if not sly.fs.file_exists(curr_im_path):
+                    logger.warn('There is no image {} in {} folder, but it must be. Video will be skipped.'.format(
+                        curr_ovis_im_path.split('/')[1], video_name))
+                    no_image = True
+                    break
+                video.write(cv2.imread(curr_im_path))
+            if no_image:
+                continue
+            progress.iter_done_report()
+            video.release()
+            # =======================================================================================================
+            frames = []
+            for idx in range(len(images)):
+                figures = []
+                for fig_id, curr_ann in enumerate(curr_anns):
+                    ovis_geom = curr_ann[2][idx]
+                    if ovis_geom:
+                        mask = decode(ovis_geom).astype(bool)
+                        if img_size[1] % 2 == 1:
+                            mask[mask.shape[0] - 1, :] = False
+                        if img_size[0] % 2 == 1:
+                            mask[:, mask.shape[1] - 1] = False
+                        geom = sly.Bitmap(mask)
+                        figure = sly.VideoFigure(video_objects[curr_ann[1]], geom, idx)
+                        figures.append(figure)
+                new_frame = sly.Frame(idx, figures)
+                frames.append(new_frame)
+
+            file_info = api.video.upload_paths(new_dataset.id, [video_name], [video_path])
+            new_frames_collection = sly.FrameCollection(frames)
+            new_objects = sly.VideoObjectCollection(list(video_objects.values()))
+            if random.random() < samplePercent:
+                tag = VideoTag(tag_meta_val)
+            else:
+                tag = VideoTag(tag_meta_train)
+            tag_collection = VideoTagCollection([tag])
+            ann = sly.VideoAnnotation((img_size[1], img_size[0]), len(frames), objects=new_objects,
+                                      frames=new_frames_collection, tags=tag_collection)
+            logger.info('Create annotation for video {}'.format(video_name))
+            api.video.annotation.append(file_info[0].id, ann)
+
+    my_app.stop()
+
+
+def main():
+    sly.logger.info("Script arguments", extra={
+        "TEAM_ID": TEAM_ID,
+        "WORKSPACE_ID": WORKSPACE_ID
+    })
+    my_app.run(initial_events=[{"command": "import_davis"}])
+
+
+if __name__ == '__main__':
+    sly.main_wrapper("main", main)
+
